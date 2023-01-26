c444c8cc735c72bc022f18635e12372b053d8559
Ankur Pata <ankur.patankar@timedoctor.com>
2023-01-26 11:36:43 +0400

    Removing Redis session

--- src/app.js
+++ src/app.js
@@ -8,21 +8,10 @@ const passport = require('passport');
 const httpStatus = require('http-status');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
-const redis = require('redis');
-const cookies = require("cookie-parser");
-const session = require('express-session');
-
-const redisClient = redis.createClient({
-  host: 'localhost',
-  port: 6379,
-  legacyMode: true
-});
-const RedisStore = require('connect-redis')(session);
-
-const {jwtStrategy} = require('./config/passport');
-const {authLimiter} = require('./middlewares/rateLimiter');
+const { jwtStrategy } = require('./config/passport');
+const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
-const {errorConverter, errorHandler} = require('./middlewares/error');
+const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 
 const app = express();
@@ -35,31 +24,11 @@ if (config.env !== 'test') {
 // set security HTTP headers
 app.use(helmet());
 
-//Use cookies
-app.use(cookies());
-
-// Use Redis session
-redisClient.on('error', function (err) {
-  console.log('Could not establish a connection with redis. ' + err);
-});
-redisClient.connect().catch(console.error)
-
-redisClient.on('connect', function (err) {
-  console.log('Connected to redis successfully');
-});
-
-app.use(
-  session({
-    name: 'PHPSESSID',
-    secret: 'ms',
-    store: new RedisStore({ client: redisClient, prefix: 'PHPSESSID' }),
-  })
-);
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // sanitize request data
 app.use(xss());
@@ -86,7 +55,6 @@ app.use('/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  console.log(req.url, 'not found req url');
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
